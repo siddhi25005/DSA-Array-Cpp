51a078041753881d62da72729b14711d764a5a97
siddhi25005 <tiwarisidddhi2005@gmail.com>
2026-01-07 19:10:34 +0530

    Create move_zeros_swap_two_pointer.cpp

---
+++ move_zeros_swap_two_pointer.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int>move(vector<int>&arr) {
+    int n=arr.size();
+    int p=0;
+    for(int i=0;i<n;i++) {
+        if(arr[i]!=0) {
+            swap(arr[i],arr[p]);
+            p++;
+        }
+    }
+    return arr;
+}
+int main() {
+    int n;
+    cout<<"enter size of array :";
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) {
+        cout<<"enter array :";
+        cin>>arr[i];
+    }
+    vector<int>arr1=move(arr);
+    for(int i=0;i<n;i++) {
+        cout<<arr1[i]<<"\t";
+    }
+}
